9c1f6d373b8de3cbf58eff0c647822d17957c7e3
Cangsfckcpu <Cangs_fckcpu@163.com>
2018-10-11 11:11:41 +0800

    刘笑天 20181011 11:10:00

---
+++ generate/src/main/java/com/adc/da/generate/util/ExamineeinvolunteerinformationEOPrompt.java
@@ -0,0 +1,12 @@
+package com.adc.da.generate.util;
+
+import com.adc.da.myutil.util.PublicPrompt;
+
+public class ExamineeinvolunteerinformationEOPrompt extends PublicPrompt{
+    public static final String SCHOOL_SIGNED = "您已报考该学校";
+    public static final String SCHOOL_NOTSIGNED = "您未报考该学校，可以报考";
+    public static final String BATCHDELETE_SUCCESS = "批量删除成功";
+    public static final String APPLY_SEVEN = "已申报7条志愿";
+    public static final String APPLY_SUCCESS = "申报成功";
+}
+
