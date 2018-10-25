6f25a327453683246faa3f7f828965e8bbe1b97b
Mr.Liu <lzj470612639@outlook.com>
2018-10-25 20:46:38 +0800

    考生信息录入over

--- generate/src/main/java/com/adc/da/generate/service/ExamineeinformationEOService.java
+++ generate/src/main/java/com/adc/da/generate/service/ExamineeinformationEOService.java
@@ -42,29 +42,30 @@ public class ExamineeinformationEOService extends BaseService<Examineeinformatio
     }
 
     /**
-    * @Description:    根据考生id获取相应的内容
-    * @Author:         xwb
-    * @CreateDate:     2018/10/25 17:20
-    * @Version:        1.0
-    */
-    public List<ExamineeinformationVO> selectExamineeinfomation(String examineekey){
+     * @Description: 根据考生id获取相应的内容
+     * @Author: xwb
+     * @CreateDate: 2018/10/25 17:20
+     * @Version: 1.0
+     */
+    public List<ExamineeinformationVO> selectExamineeinfomation(String examineekey) {
         return dao.selectByExamineeKey(examineekey);
     }
+
     /**
-    * @Description:    查询录入信息
-    * @Author:         xwb
-    * @CreateDate:     2018/10/15 9:36
-    * @Version:        1.0
-    */
+     * @Description: 查询录入信息
+     * @Author: xwb
+     * @CreateDate: 2018/10/15 9:36
+     * @Version: 1.0
+     */
     public PageInfo<ExamineeinformationVO> selectExamineeinformationByPage(BasePage page) throws Exception {
         Integer rowCount = this.queryAdminssionByCount(page);
         page.getPager().setRowCount(rowCount);
         PageInfo<ExamineeinformationVO> pageInfo = new PageInfo();
         pageInfo.setList(this.dao.examineeinformationByPage(page));
 //        pageInfo.setCount((long)page.getPager().getRowCount());
-        pageInfo.setCount((long)rowCount);
+        pageInfo.setCount((long) rowCount);
         pageInfo.setPageSize(page.getPager().getPageSize());
-        pageInfo.setPageCount((long)page.getPager().getPageCount());
+        pageInfo.setPageCount((long) page.getPager().getPageCount());
         pageInfo.setPageNo(page.getPager().getPageId());
         return pageInfo;
     }
@@ -80,9 +81,9 @@ public class ExamineeinformationEOService extends BaseService<Examineeinformatio
      * @param examineeinformationEO 考生信息
      * @return
      */
-    public String informationCheck(ExamineeinformationEO examineeinformationEO,UserinformationEO userinformationEO) {
+    public String informationCheck(ExamineeinformationEO examineeinformationEO, UserinformationEO userinformationEO) {
         //非空 校验
-            if (examineeinformationEO.getQuasiexaminationnumber() == null ||
+        if (examineeinformationEO.getQuasiexaminationnumber() == null ||
                 examineeinformationEO.getRealname() == null ||
                 examineeinformationEO.getSex() == null ||
                 examineeinformationEO.getAge() == null ||
@@ -92,63 +93,82 @@ public class ExamineeinformationEOService extends BaseService<Examineeinformatio
                 examineeinformationEO.getNativeplace() == null ||
                 examineeinformationEO.getEmail() == null ||
                 examineeinformationEO.getPhonenumber() == null ||
-                examineeinformationEO.getGraduateschool() == null||
-                userinformationEO.getUseraccount()==null||
-                userinformationEO.getUserpassword()==null
+                examineeinformationEO.getGraduateschool() == null ||
+                userinformationEO.getUseraccount() == null ||
+                userinformationEO.getUserpassword() == null
                 ) {
             return ExamineeinformationEOPrompt.NOT_NULL;
-        }
-        //身份证号 重复校验
-        if (dao.queryIdCard(examineeinformationEO.getIdcardnumber())!=0) {
-            return ExamineeinformationEOPrompt.IDCARD_REPEAT;
         }
         //身份证号 长度18位 校验
         if (examineeinformationEO.getIdcardnumber().trim().length() != 18) {
             return ExamineeinformationEOPrompt.IDCARD_LENGTH;
         }
+
+        ExamineeinformationEO examineeInfo = dao.selectByPrimaryKey(examineeinformationEO.getExamineekey());
+        //身份证号 重复校验
+        if (!examineeInfo.getIdcardnumber().equals(examineeinformationEO.getIdcardnumber())) { //不是本人的身份证号
+            if (dao.queryIdCard(examineeinformationEO.getIdcardnumber()) != 0) {
+                return ExamineeinformationEOPrompt.IDCARD_REPEAT;
+            }
+        }
+
+
         //手机号 重复校验
-        if (dao.queryPhoneNumber(examineeinformationEO.getPhonenumber())!=0) {
-            return ExamineeinformationEOPrompt.PHONENUMBER_REPEAT;
+        if (!examineeInfo.getPhonenumber().equals(examineeinformationEO.getPhonenumber())) {//不是本人的手机号
+            if (dao.queryPhoneNumber(examineeinformationEO.getPhonenumber()) != 0) {
+                return ExamineeinformationEOPrompt.PHONENUMBER_REPEAT;
+            }
         }
+
         //邮箱 重复校验
-        if (dao.queryEMail(examineeinformationEO.getEmail())!=0) {
-            return ExamineeinformationEOPrompt.EMAIL_REPEAT;
+        if (!examineeInfo.getEmail().equals(examineeinformationEO.getEmail())) {//不是本人的邮箱
+            if (dao.queryEMail(examineeinformationEO.getEmail()) != 0) {
+                return ExamineeinformationEOPrompt.EMAIL_REPEAT;
+            }
         }
+
+
         return "true";
     }
+
     /**
      * 注册查重
      * 刘笑天 20181011
+     *
      * @param examineeinformationEO
      * @return
      */
-    public ExamineeinformationEO checkRegistInfo(ExamineeinformationEO examineeinformationEO){
+    public ExamineeinformationEO checkRegistInfo(ExamineeinformationEO examineeinformationEO) {
         return dao.checkRegistInfo(examineeinformationEO);
     }
 
     /**
      * 准考证验证
      * 刘笑天 20181019
+     *
      * @param quasiExaminationNumber
      */
-    public ExamineeinformationEO checkQuasiExaminationNumber(String quasiExaminationNumber){
+    public ExamineeinformationEO checkQuasiExaminationNumber(String quasiExaminationNumber) {
         return dao.checkQuasiExaminationNumber(quasiExaminationNumber);
     }
 
     /**
      * 注册_考生账号注册
      * 刘笑天 20181022
+     *
      * @param userinformationEO
      */
-    public void  addUserInformation(UserinformationEO userinformationEO){
+    public void addUserInformation(UserinformationEO userinformationEO) {
         dao.addUserInformation(userinformationEO);
     }
+
     /**
      * 注册_考生信息关联账号
      * 刘笑天 20181022
+     *
      * @param examineeinformationEO
      */
-    public void addExamineeInformation(ExamineeinformationEO examineeinformationEO){
+    public void addExamineeInformation(ExamineeinformationEO examineeinformationEO) {
         dao.addExamineeInformation(examineeinformationEO);
     }
 }
